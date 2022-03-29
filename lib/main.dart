09ad76b975087e11ae35998ce6d03b6ef4d77858
Norbek <norbekuktamov@gmail.com>
2022-03-29 11:16:12 +0500

    Update sign up page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:databasedemo/pages/home_page.dart';
+import 'package:databasedemo/pages/sign_up_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive/hive.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home:Homepage(),
+      home:SignUpPage(),
     );
   }
 }
--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,17 +1,245 @@
+import 'package:databasedemo/pages/sign_up_page.dart';
 import 'package:flutter/material.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({Key? key}) : super(key: key);
+  static final id="sign_in_page";
 
   @override
   State<SignInPage> createState() => _SignInPageState();
 }
 
 class _SignInPageState extends State<SignInPage> {
+
+  TextEditingController emailController=  TextEditingController();
+  TextEditingController passwordController=  TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.grey.shade100,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            SizedBox(height: 50),
+            Center(
+              child:Container(
+                height: 170,
+                child: Image(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/ic_image.png"),
+                ),
+              ),
+            ),
 
+            //#login,#welcome
+            Padding(
+              padding: EdgeInsets.all(30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Welcome back!",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Login in to your existant account of Q Alurre",
+                    style: TextStyle(color: Colors.grey.shade500, fontSize: 15),
+                  )
+                ],
+              ),
+            ),
+
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade100,
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        //#Login
+                        //email
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            color: Colors.white,
+                          ),
+                          child: TextField(
+                            controller: emailController,
+                            style: TextStyle(color: Colors.blue),
+                            decoration: InputDecoration(
+                              icon: Icon(
+                                Icons.person,
+                              ),
+                              hintText: "Email",
+                              hintStyle: TextStyle(color: Colors.grey.shade400),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    color: Colors.blue, width: 2.0),
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+
+                        //Password
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            color: Colors.white,
+                          ),
+                          child: TextField(
+                            controller: passwordController,
+                            style: TextStyle(color: Colors.blue),
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.lock),
+                              hintText: "Password",
+                              hintStyle: TextStyle(color: Colors.grey.shade400),
+                              border: InputBorder.none,
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                    color: Colors.blue, width: 2.0),
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          margin: EdgeInsets.only(left: 200),
+                          child: Text(
+                            'Forgot password?',
+                          ),
+                        ),
+
+                        SizedBox(
+                          height: 15,
+                        ),
+
+                        //#Create
+                        FlatButton(
+                          onPressed: () {
+
+                          },
+                          child: Container(
+                            margin: EdgeInsets.symmetric(horizontal: 60),
+                            height: 50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.blue.shade900,
+                            ),
+                            child: Center(
+                              child: Text(
+                                "LOG IN",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17),
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            'Or connect using',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.grey.shade500),
+                          ),
+                        ),
+
+                        //#Bottom
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                              child: Text('Facebook'),
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.indigo.shade800,
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 32, vertical: 13),
+                                  textStyle: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            ElevatedButton(
+                              child: Text('Google'),
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  primary: Colors.red,
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 40, vertical: 13),
+                                  textStyle: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Don't have an account?",
+                              style: TextStyle(
+                                  color: Colors.grey.shade800, fontSize: 15),
+                            ),
+                            FlatButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context,SignUpPage.id);
+                              },
+                              child: Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
